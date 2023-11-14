ea960160322f9ea6fc9ee490e477dca266e2e846
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2023-11-14 22:14:19 +0100

    Add home page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const router = express.Router();
 
+const messages = [
+  {
+    text: 'Welcome to my message board!',
+    author: 'McStanley',
+    date: new Date(),
+  },
+  {
+    text: 'Feel free to leave a message!',
+    author: 'McStanley',
+    date: new Date(),
+  },
+];
+
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages });
 });
 
 module.exports = router;
